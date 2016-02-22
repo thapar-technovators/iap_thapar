7bd35d77eda1d72883f9c69693cc5e12dcb70ec9
akshit.arora1995@gmail.com <akshit.arora1995@gmail.com>
2016-02-22 21:08:10 +0530

    testing gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -71,7 +71,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | the query builder class.
 testing gitignore
 */
-$active_group = 'default';
+$active_group = 'akshit';
 $query_builder = TRUE;
 
 $db['default'] = array(
