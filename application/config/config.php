a62eca4767ffde4c621042f37482a3bffee4ff38
Arush Nagpal <arushngpl16@gmail.com>
2016-02-22 12:06:04 +0530

    Added different database configurations

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://developerarush.esy.es/';
+//$config['base_url'] = 'http://developerarush.esy.es/';
+$config['base_url'] = 'http://localhost/iap_thapar';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +36,7 @@ $config['base_url'] = 'http://developerarush.esy.es/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
