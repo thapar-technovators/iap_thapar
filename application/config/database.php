f2ac28bad31467fc849c8e606ce5c1c275d4a0b6
akshit.arora1995@gmail.com <akshit.arora1995@gmail.com>
2016-02-22 20:36:12 +0530

    akshit local db settings updated

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'arush';
+$active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -100,6 +100,27 @@ $db['arush'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '1225',
+	'database' => 'iap_thapar',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+$db['akshti'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'password',
 	'database' => 'iap_thapar',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
