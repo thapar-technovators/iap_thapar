5accb3916d43dfd5ebba024341487e53cea855fc
Arush Nagpal <arushngpl16@gmail.com>
2016-02-24 17:55:37 +0530

    Dashboard view error debug and routed default controller to run the dashboard

--- application/views/admin/admin_footer.php
+++ application/views/admin/admin_footer.php
@@ -8,43 +8,43 @@
             <!--END PAGE WRAPPER-->
         </div>
     </div>
-    <script src="script/jquery-1.10.2.min.js"></script>
-    <script src="script/jquery-migrate-1.2.1.min.js"></script>
-    <script src="script/jquery-ui.js"></script>
-    <script src="script/bootstrap.min.js"></script>
-    <script src="script/bootstrap-hover-dropdown.js"></script>
-    <script src="script/html5shiv.js"></script>
-    <script src="script/respond.min.js"></script>
-    <script src="script/jquery.metisMenu.js"></script>
-    <script src="script/jquery.slimscroll.js"></script>
-    <script src="script/jquery.cookie.js"></script>
-    <script src="script/icheck.min.js"></script>
-    <script src="script/custom.min.js"></script>
-    <script src="script/jquery.news-ticker.js"></script>
-    <script src="script/jquery.menu.js"></script>
-    <script src="script/pace.min.js"></script>
-    <script src="script/holder.js"></script>
-    <script src="script/responsive-tabs.js"></script>
-    <script src="script/jquery.flot.js"></script>
-    <script src="script/jquery.flot.categories.js"></script>
-    <script src="script/jquery.flot.pie.js"></script>
-    <script src="script/jquery.flot.tooltip.js"></script>
-    <script src="script/jquery.flot.resize.js"></script>
-    <script src="script/jquery.flot.fillbetween.js"></script>
-    <script src="script/jquery.flot.stack.js"></script>
-    <script src="script/jquery.flot.spline.js"></script>
-    <script src="script/zabuto_calendar.min.js"></script>
-    <script src="script/index.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery-1.10.2.min.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery-migrate-1.2.1.min.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery-ui.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/bootstrap-hover-dropdown.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/html5shiv.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/respond.min.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.metisMenu.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.slimscroll.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.cookie.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/icheck.min.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/custom.min.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.news-ticker.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.menu.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/pace.min.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/holder.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/responsive-tabs.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.flot.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.flot.categories.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.flot.pie.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.flot.tooltip.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.flot.resize.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.flot.fillbetween.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.flot.stack.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/jquery.flot.spline.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/zabuto_calendar.min.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/index.js"></script>
     <!--LOADING SCRIPTS FOR CHARTS-->
-    <script src="script/highcharts.js"></script>
-    <script src="script/data.js"></script>
-    <script src="script/drilldown.js"></script>
-    <script src="script/exporting.js"></script>
-    <script src="script/highcharts-more.js"></script>
-    <script src="script/charts-highchart-pie.js"></script>
-    <script src="script/charts-highchart-more.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/highcharts.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/data.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/drilldown.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/exporting.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/highcharts-more.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/charts-highchart-pie.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/charts-highchart-more.js"></script>
     <!--CORE JAVASCRIPT-->
-    <script src="script/main.js"></script>
+    <script src="<?php echo base_url();?>dashboard/script/main.js"></script>
     <script>  
 </script>
 </body>
--- application/views/admin/admin_header.php
+++ application/views/admin/admin_header.php
@@ -5,23 +5,23 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="images/icons/favicon.ico">
-    <link rel="apple-touch-icon" href="images/icons/favicon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="images/icons/favicon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="images/icons/favicon-114x114.png">
+    <link rel="shortcut icon" href="<?php echo base_url();?>dashboard/images/icons/favicon.ico">
+    <link rel="apple-touch-icon" href="<?php echo base_url();?>dashboard/images/icons/favicon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url();?>dashboard/images/icons/favicon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url();?>dashboard/images/icons/favicon-114x114.png">
     <!--Loading bootstrap css-->
     <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700">
     <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700,300">
-    <link type="text/css" rel="stylesheet" href="styles/jquery-ui-1.10.4.custom.min.css">
-    <link type="text/css" rel="stylesheet" href="styles/font-awesome.min.css">
-    <link type="text/css" rel="stylesheet" href="styles/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="styles/animate.css">
-    <link type="text/css" rel="stylesheet" href="styles/all.css">
-    <link type="text/css" rel="stylesheet" href="styles/main.css">
-    <link type="text/css" rel="stylesheet" href="styles/style-responsive.css">
-    <link type="text/css" rel="stylesheet" href="styles/zabuto_calendar.min.css">
-    <link type="text/css" rel="stylesheet" href="styles/pace.css">
-    <link type="text/css" rel="stylesheet" href="styles/jquery.news-ticker.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/jquery-ui-1.10.4.custom.min.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/animate.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/all.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/main.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/style-responsive.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/zabuto_calendar.min.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/pace.css">
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>dashboard/styles/jquery.news-ticker.css">
 </head>
 <body>
     <div>
@@ -232,7 +232,7 @@
                             </li>
                             <li class="odd">
                                 <p>
-                                    <img src="images/avatar/48.jpg" class="avt" /><span class="user">Swlabs</span><span
+                                    <img src="<?php echo base_url();?>dashboard/images/avatar/48.jpg" class="avt" /><span class="user">Swlabs</span><span
                                         class="time">09:33</span></p>
                                 <p>
                                     Hi, we're listening you...</p>
