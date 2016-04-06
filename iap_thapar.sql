c69f2e8948cec964a737e538d7b2ebdf19a878b1
Chahak Gupta <chahakgupta4@gmail.com>
2016-04-07 00:14:06 +0530

    updated student table

--- iap_thapar.sql
+++ iap_thapar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2016 at 10:48 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Apr 06, 2016 at 08:43 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.5.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `iap_thapar`
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `administrator`
 --
 
-CREATE TABLE IF NOT EXISTS `administrator` (
+CREATE TABLE `administrator` (
   `registration_id` varchar(100) NOT NULL,
   `name` varchar(200) NOT NULL,
   `password` varchar(250) NOT NULL
@@ -45,10 +45,10 @@ INSERT INTO `administrator` (`registration_id`, `name`, `password`) VALUES
 -- Table structure for table `branch`
 --
 
-CREATE TABLE IF NOT EXISTS `branch` (
+CREATE TABLE `branch` (
   `id` bigint(20) NOT NULL,
   `branch` varchar(200) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `branch`
@@ -64,7 +64,7 @@ INSERT INTO `branch` (`id`, `branch`) VALUES
 -- Table structure for table `faculty`
 --
 
-CREATE TABLE IF NOT EXISTS `faculty` (
+CREATE TABLE `faculty` (
   `registration_id` bigint(20) NOT NULL,
   `initials` varchar(10) NOT NULL,
   `name` varchar(250) NOT NULL,
@@ -78,20 +78,13 @@ CREATE TABLE IF NOT EXISTS `faculty` (
   `access_token` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `faculty`
---
-
-INSERT INTO `faculty` (`registration_id`, `initials`, `name`, `password`, `phone`, `email`, `designation`, `pref1`, `pref2`, `pref3`, `access_token`) VALUES
-(101303012, 'Dr.', 'Akshit Arora', '$2y$10$wanttocrackitokaythenOltd1du7MuUHDAvjpIFrIYr6xXmZzyyq', 7696061995, 'akshit.arora1995@gmail.com', 'Professor', NULL, NULL, NULL, NULL);
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `mentor`
 --
 
-CREATE TABLE IF NOT EXISTS `mentor` (
+CREATE TABLE `mentor` (
   `initials` varchar(10) NOT NULL,
   `name` varchar(250) NOT NULL,
   `phone` bigint(20) NOT NULL,
@@ -114,7 +107,7 @@ INSERT INTO `mentor` (`initials`, `name`, `phone`, `email`, `company`, `password
 -- Table structure for table `student`
 --
 
-CREATE TABLE IF NOT EXISTS `student` (
+CREATE TABLE `student` (
   `roll_number` bigint(20) NOT NULL,
   `email` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
@@ -122,18 +115,18 @@ CREATE TABLE IF NOT EXISTS `student` (
   `branch` varchar(100) NOT NULL,
   `semester` int(2) NOT NULL,
   `phone` bigint(20) NOT NULL,
-  `time_of_registration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `time_of_registration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activation_link` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`roll_number`, `email`, `password`, `name`, `branch`, `semester`, `phone`, `time_of_registration`) VALUES
-(101303004, 'abhinavgarg017@gmail.com', '$2y$10$wanttocrackitokaythenOltd1du7MuUHDAvjpIFrIYr6xXmZzyyq', 'Abhinav Garg', 'Computer Engineering', 7, 99999999999, '2016-04-02 07:02:54'),
-(101303012, 'akshit.arora1995@gmail.com', '$2y$10$wanttocrackitokaythenOltd1du7MuUHDAvjpIFrIYr6xXmZzyyq', 'Akshit Arora', 'Computer Engineering', 8, 7696061995, '2016-04-05 20:05:33'),
-(101303034, 'arushngpl16@gmail.com', '$2y$10$wanttocrackitokaythenOltd1du7MuUHDAvjpIFrIYr6xXmZzyyq', 'Arush Nagpal', 'Mechanical Engineering', 4, 9988090859, '2016-03-04 18:19:44'),
-(101303041, 'chahakgupta4@gmail.com', '$2y$10$wanttocrackitokaythenOltd1du7MuUHDAvjpIFrIYr6xXmZzyyq', 'Chahak', 'Computer Engineering', 6, 9041114525, '2016-04-05 10:45:39');
+INSERT INTO `student` (`roll_number`, `email`, `password`, `name`, `branch`, `semester`, `phone`, `time_of_registration`, `activation_link`) VALUES
+(101303004, 'abhinavgarg017@gmail.com', '$2y$10$wanttocrackitokaythenOmTvjQXqEWBtXxizGg3Z14X/wXLFGH..', 'Abhinav Garg', 'Computer Engineering', 7, 99999999999, '2016-04-02 07:02:54', 0),
+(101303034, 'arushngpl16@gmail.com', '$2y$10$wanttocrackitokaythenOmTvjQXqEWBtXxizGg3Z14X/wXLFGH..', 'Arush Nagpal', 'Mechanical Engineering', 4, 9988090859, '2016-03-04 18:19:44', 0),
+(101303041, 'chahakgupta4@gmail.com', '$2y$10$wanttocrackitokaythenOmTvjQXqEWBtXxizGg3Z14X/wXLFGH..', 'Chahak', 'Computer Engineering', 6, 9041114525, '2016-04-05 10:45:39', 0);
 
 -- --------------------------------------------------------
 
@@ -141,7 +134,7 @@ INSERT INTO `student` (`roll_number`, `email`, `password`, `name`, `branch`, `se
 -- Table structure for table `training_data`
 --
 
-CREATE TABLE IF NOT EXISTS `training_data` (
+CREATE TABLE `training_data` (
   `td_id` int(11) NOT NULL,
   `roll_number` bigint(20) NOT NULL,
   `company` varchar(250) DEFAULT NULL,
@@ -150,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `training_data` (
   `months` int(11) NOT NULL,
   `phase` int(11) NOT NULL DEFAULT '0',
   `admin_approve` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `training_data`
@@ -194,7 +187,8 @@ ALTER TABLE `mentor`
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-  ADD PRIMARY KEY (`roll_number`), ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`roll_number`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `training_data`
@@ -210,12 +204,12 @@ ALTER TABLE `training_data`
 -- AUTO_INCREMENT for table `branch`
 --
 ALTER TABLE `branch`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `training_data`
 --
 ALTER TABLE `training_data`
-  MODIFY `td_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `td_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
