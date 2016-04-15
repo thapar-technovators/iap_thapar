9ed44f9bc51c2e6ec3da0caad5e94e8d8616d294
abhi017 <abhinavgarg017@gmail.com>
2016-04-16 00:11:27 +0530

    Db update
    in mentor table one column added

--- iap_thapar.sql
+++ iap_thapar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2016 at 11:03 AM
--- Server version: 10.1.10-MariaDB
--- PHP Version: 5.5.30
+-- Generation Time: Apr 15, 2016 at 08:36 PM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `administrator`
 --
 
-CREATE TABLE `administrator` (
+CREATE TABLE IF NOT EXISTS `administrator` (
   `registration_id` varchar(100) NOT NULL,
   `name` varchar(200) NOT NULL,
   `password` varchar(250) NOT NULL
@@ -45,10 +45,10 @@ INSERT INTO `administrator` (`registration_id`, `name`, `password`) VALUES
 -- Table structure for table `branch`
 --
 
-CREATE TABLE `branch` (
+CREATE TABLE IF NOT EXISTS `branch` (
   `id` bigint(20) NOT NULL,
   `branch` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `branch`
@@ -64,7 +64,7 @@ INSERT INTO `branch` (`id`, `branch`) VALUES
 -- Table structure for table `faculty`
 --
 
-CREATE TABLE `faculty` (
+CREATE TABLE IF NOT EXISTS `faculty` (
   `registration_id` bigint(20) NOT NULL,
   `initials` varchar(10) NOT NULL,
   `name` varchar(250) NOT NULL,
@@ -84,22 +84,23 @@ CREATE TABLE `faculty` (
 -- Table structure for table `mentor`
 --
 
-CREATE TABLE `mentor` (
+CREATE TABLE IF NOT EXISTS `mentor` (
   `initials` varchar(10) NOT NULL,
   `name` varchar(250) NOT NULL,
   `phone` bigint(20) NOT NULL,
   `email` varchar(200) NOT NULL,
   `company` varchar(100) NOT NULL,
-  `password` varchar(250) NOT NULL
+  `password` varchar(250) NOT NULL,
+  `activation_link` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `mentor`
 --
 
-INSERT INTO `mentor` (`initials`, `name`, `phone`, `email`, `company`, `password`) VALUES
-('Mr.', 'Abhinav Garg', 9803360828, 'abhinavgarg017@gmail.com', 'microsoft', '$2y$10$wanttocrackitokaythenOn8pJMiMInc6e/YOlGdfZz2aJbpqROoq'),
-('Mr.', 'Arush Nagpal', 9803360828, 'arushngpl16@gmail.com', 'microsoft', '$2y$10$wanttocrackitokaythenOQlgZiefbu9OzjU873v9LbZOMZkARSIm');
+INSERT INTO `mentor` (`initials`, `name`, `phone`, `email`, `company`, `password`, `activation_link`) VALUES
+('Mr.', 'Abhinav Garg', 9803360828, 'abhinavgarg017@gmail.com', 'microsoft', '$2y$10$wanttocrackitokaythenOn8pJMiMInc6e/YOlGdfZz2aJbpqROoq', '0'),
+('Mr.', 'Arush Nagpal', 9803360828, 'arushngpl16@gmail.com', 'microsoft', '$2y$10$wanttocrackitokaythenOn8pJMiMInc6e/YOlGdfZz2aJbpqROoq', '0');
 
 -- --------------------------------------------------------
 
@@ -107,7 +108,7 @@ INSERT INTO `mentor` (`initials`, `name`, `phone`, `email`, `company`, `password
 -- Table structure for table `student`
 --
 
-CREATE TABLE `student` (
+CREATE TABLE IF NOT EXISTS `student` (
   `roll_number` bigint(20) NOT NULL,
   `email` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
@@ -134,7 +135,7 @@ INSERT INTO `student` (`roll_number`, `email`, `password`, `name`, `branch`, `se
 -- Table structure for table `training_data`
 --
 
-CREATE TABLE `training_data` (
+CREATE TABLE IF NOT EXISTS `training_data` (
   `td_id` int(11) NOT NULL,
   `roll_number` bigint(20) NOT NULL,
   `company` varchar(250) DEFAULT NULL,
@@ -144,7 +145,7 @@ CREATE TABLE `training_data` (
   `mentor` varchar(200) NOT NULL,
   `phase` int(11) NOT NULL DEFAULT '0',
   `admin_approve` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `training_data`
@@ -154,7 +155,7 @@ INSERT INTO `training_data` (`td_id`, `roll_number`, `company`, `city`, `date_of
 (1, 101303041, 'ABC', 'Bangalore', '0000-00-00', 0, 'abhinavgarg017@gmail.com', 0, 1),
 (2, 101303042, 'BCD', 'Pune', '0000-00-00', 0, '', 0, 1),
 (3, 101303034, 'GHI', 'New Delhi', '0000-00-00', 0, '', 0, 1),
-(4, 101303041, 'BCD', 'Pune', '2016-04-13', 2, '', 0, 1);
+(4, 101303041, 'BCD', 'Pune', '2016-04-13', 2, 'arushngpl16@gmail.com', 0, 1);
 
 --
 -- Indexes for dumped tables
@@ -205,12 +206,12 @@ ALTER TABLE `training_data`
 -- AUTO_INCREMENT for table `branch`
 --
 ALTER TABLE `branch`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `training_data`
 --
 ALTER TABLE `training_data`
-  MODIFY `td_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `td_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
